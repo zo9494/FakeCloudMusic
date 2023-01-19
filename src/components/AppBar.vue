550f8eaa331e307ff9abb3363a54521165f4b26f
zo9494 <zhonghao.coder@outlook.com>
2023-01-19 23:30:04 +0800

    🐞 fix(AppBar.vue): 修复窗口按钮不能点击

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app_bar_place">
+  <div :class="`app_bar_place ${dragClassName}`">
     <div></div>
     <div></div>
   </div>
@@ -9,19 +9,25 @@
 <script lang="ts" setup>
 import WinTitleBar from './WinTitleBar.vue';
 let appBar;
+let dragClassName = 'drag'
 if (process.platform === 'win32') {
   appBar = WinTitleBar;
+  dragClassName = ''
 }
 console.log(process);
 </script>
 <style lang="scss" scoped>
 .app_bar_place {
   display: grid;
+
   grid-template-columns: 160px auto;
-  -webkit-app-region: drag;
 
   >div:nth-of-type(1) {
     background-color: #ededed;
   }
+}
+
+.drag {
+  -webkit-app-region: drag;
 }
 </style>
