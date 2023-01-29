80b5bb4cb4fa6ffb56fafe6b7e1f87fc6ffe3c3c
zo9494 <zhonghao.coder@outlook.com>
2023-01-30 00:56:30 +0800

    🦄 refactor(index.ts): 分离代码

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,3 +1,4 @@
+import '../utils/env';
 import {
   app,
   BrowserWindow,
@@ -12,25 +13,9 @@ import server = require('NeteaseCloudMusicApi/server');
 import { EVENT } from '../utils/eventTypes';
 import { isDevelopment, isLinux, isMac, isWin } from '../utils/platform';
 
-// The built directory structure
-//
-// ├─┬ dist-electron
-// │ ├─┬ main
-// │ │ └── index.js    > Electron-Main
-// │ └─┬ preload
-// │   └── index.js    > Preload-Scripts
-// ├─┬ dist
-// │ └── index.html    > Electron-Renderer
-//
-process.env.DIST_ELECTRON = join(__dirname, '..');
-process.env.DIST = join(process.env.DIST_ELECTRON, '../dist');
-process.env.PUBLIC = process.env.VITE_DEV_SERVER_URL
-  ? join(process.env.DIST_ELECTRON, '../public')
-  : process.env.DIST;
-
 // Disable GPU Acceleration for Windows 7
 if (release().startsWith('6.1')) app.disableHardwareAcceleration();
-
+import { Login } from './login';
 // Set application name for Windows 10+ notifications
 if (isWin) app.setAppUserModelId(app.getName());
 
@@ -44,229 +29,155 @@ if (!app.requestSingleInstanceLock()) {
 // Read more on https://www.electronjs.org/docs/latest/tutorial/security
 // process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 
+// Here, you can also use other preload
+const preload = join(__dirname, '../preload/index.js');
+
+const url = process.env.VITE_DEV_SERVER_URL;
+
+const indexHtml = join(process.env.DIST, 'index.html');
+
+const vue_dev = join(process.cwd(), '/vue_devtools/');
+
 class Main {
-  win: BrowserWindow | null;
-  neteaseApi: any;
-  constructor() {}
-  init() {}
-  async createWindow() {
-    const win = new BrowserWindow({
+  static win: BrowserWindow | null = null;
+  static neteaseApi: any;
+  constructor() {
+    app.whenReady().then(() => {
+      this.createServer();
+      this.createWindow();
+      this.registerAppEvent();
+      this.registerHandle();
+      this.registerGlobalShortcut();
+    });
+  }
+  private async createWindow() {
+    Main.win = new BrowserWindow({
       webPreferences: {
         preload,
         nodeIntegration: true,
       },
       frame: isMac,
+      width: 900,
+      height: 600,
+      minWidth: 900,
+      minHeight: 600,
+      titleBarStyle: 'hiddenInset',
+      trafficLightPosition: { x: 5, y: 5 },
     });
-
     if (process.env.VITE_DEV_SERVER_URL) {
-      // electron-vite-vue#298
-      await win.loadURL(url);
-      // Open devTool if the app is not packaged
-      win.webContents.openDevTools();
+      await Main.win.loadURL(url);
+      // open devtools
+      if (isDevelopment) {
+        Main.win.webContents.openDevTools();
+      }
     } else {
-      win.loadFile(indexHtml);
+      Main.win.loadFile(indexHtml);
     }
 
     // Test actively push message to the Electron-Renderer
-    win.webContents.on('did-finish-load', () => {
-      win?.webContents.send(
+    Main.win.webContents.on('did-finish-load', () => {
+      Main.win?.webContents.send(
         'main-process-message',
         new Date().toLocaleString()
       );
     });
 
     // Make all links open with the browser, not with the application
-    win.webContents.setWindowOpenHandler(({ url }) => {
+    Main.win.webContents.setWindowOpenHandler(({ url }) => {
       if (url.startsWith('https:')) shell.openExternal(url);
       return { action: 'deny' };
     });
-    return win;
+    return;
   }
-  async createServer() {
+  private async createServer() {
     const { server: expressApp } = await server.serveNcmApi({
       port: 35011,
     });
-    chalk && console.log(`[NeteaseCloudMusicApi]: ${chalk.green('started')}`);
+    console.log('[NeteaseCloudMusicApi]: started');
+    Main.neteaseApi = expressApp;
+    return;
+  }
+  // 全局事件
+  private registerAppEvent() {
+    app.on('window-all-closed', () => {
+      Main.win = null;
+      if (process.platform !== 'darwin') {
+        app.quit();
+        Main.neteaseApi.close(() => {
+          console.log('[NeteaseCloudMusicApi]: closed');
+        });
+      }
+    });
 
-    return expressApp;
+    app.on('second-instance', () => {
+      const { win } = Main;
+      if (win) {
+        // Focus on the main window if the user tried to open another
+        if (win.isMinimized()) win.restore();
+        win.focus();
+      }
+    });
+
+    app.on('activate', () => {
+      const allWindows = BrowserWindow.getAllWindows();
+      if (allWindows.length) {
+        allWindows[0].focus();
+      } else {
+        new Main();
+      }
+    });
+
+    app.on('ready', async () => {
+      if (isDevelopment) {
+        try {
+          await session.defaultSession.loadExtension(vue_dev);
+          console.log(vue_dev);
+        } catch (e) {
+          console.error('Vue Devtools failed to install:', e.toString());
+        }
+      }
+    });
+  }
+  /**
+   * 全局handle
+   */
+  private registerHandle() {
+    // window
+    ipcMain.handle(EVENT.WINDOW_CLOSE, () => {
+      app.quit();
+    });
+    ipcMain.handle(EVENT.WINDOW_RESIZ, () => {
+      const { win } = Main;
+      if (win.isMaximized()) {
+        win.restore();
+      } else {
+        win.maximize();
+      }
+      return win.isMaximized();
+    });
+
+    ipcMain.handle(EVENT.WINDOW_MIN, () => {
+      Main.win.minimize();
+    });
+
+    ipcMain.handle(EVENT.LOGIN, () => {
+      new Login({ parent: Main.win });
+    });
+    ipcMain.handle(EVENT.RELOAD_USER, (_, args) => {
+      return Main.win.webContents.executeJavaScript('window.loadUser()');
+    });
+
+    Login.registerHandle();
+  }
+  /**
+   * 全局快捷键
+   */
+  private registerGlobalShortcut() {
+    globalShortcut.register('F10', () => {
+      Main.win && Main.win.webContents.openDevTools();
+      Login.win && Login.win.webContents.openDevTools();
+    });
   }
 }
 
-let win: BrowserWindow | null = null;
-let serverApp;
-// Here, you can also use other preload
-const preload = join(__dirname, '../preload/index.js');
-const preloadLogin = join(__dirname, '../preload/login.js');
-
-const url = process.env.VITE_DEV_SERVER_URL;
-
-const indexHtml = join(process.env.DIST, 'index.html');
-const loginHtml = join(process.env.DIST, '/login/index.html');
-
-const vue_dev = join(process.cwd(), '/vue_devtools/');
-
-async function createWindow() {
-  win = new BrowserWindow({
-    webPreferences: {
-      preload,
-      nodeIntegration: true,
-    },
-    frame: isMac,
-    width: 900,
-    height: 600,
-    minWidth: 900,
-    minHeight: 600,
-    titleBarStyle: 'hiddenInset',
-    trafficLightPosition: { x: 5, y: 5 },
-  });
-
-  if (process.env.VITE_DEV_SERVER_URL) {
-    await win.loadURL(url);
-    // open devtools
-    if (isDevelopment) {
-      win.webContents.openDevTools();
-    }
-  } else {
-    win.loadFile(indexHtml);
-  }
-
-  // Test actively push message to the Electron-Renderer
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString());
-  });
-
-  // Make all links open with the browser, not with the application
-  win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith('https:')) shell.openExternal(url);
-    return { action: 'deny' };
-  });
-}
-async function createServer() {
-  const { server: expressApp } = await server.serveNcmApi({
-    port: 35011,
-  });
-  console.log('[NeteaseCloudMusicApi]: started');
-  return expressApp;
-}
-
-app.whenReady().then(() => {
-  createServer().then(expressApp => {
-    serverApp = expressApp;
-  });
-  createWindow();
-
-  globalShortcut.register('F10', () => {
-    win && win.webContents.openDevTools();
-    loginWin && loginWin.webContents.openDevTools();
-  });
-});
-
-app.on('window-all-closed', () => {
-  win = null;
-  if (process.platform !== 'darwin') {
-    app.quit();
-    serverApp.close(() => {
-      console.log('[NeteaseCloudMusicApi]: closed');
-    });
-  }
-});
-
-app.on('second-instance', () => {
-  if (win) {
-    // Focus on the main window if the user tried to open another
-    if (win.isMinimized()) win.restore();
-    win.focus();
-  }
-});
-
-app.on('activate', () => {
-  const allWindows = BrowserWindow.getAllWindows();
-  if (allWindows.length) {
-    allWindows[0].focus();
-  } else {
-    createWindow();
-  }
-});
-
-app.on('ready', async () => {
-  if (isDevelopment) {
-    try {
-      await session.defaultSession.loadExtension(vue_dev);
-      console.log(vue_dev);
-    } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString());
-    }
-  }
-});
-
-// New window example arg: new windows url
-ipcMain.handle('open-win', (_, arg) => {
-  const childWindow = new BrowserWindow({
-    webPreferences: {
-      preload,
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
-
-  if (process.env.VITE_DEV_SERVER_URL) {
-    childWindow.loadURL(`${url}#${arg}`);
-  } else {
-    childWindow.loadFile(indexHtml, { hash: arg });
-  }
-});
-
-// window
-ipcMain.handle(EVENT.WINDOW_CLOSE, () => {
-  app.quit();
-});
-ipcMain.handle(EVENT.WINDOW_RESIZ, () => {
-  if (win.isMaximized()) {
-    win.restore();
-  } else {
-    win.maximize();
-  }
-  return win.isMaximized();
-});
-
-ipcMain.handle(EVENT.WINDOW_MIN, () => {
-  win.minimize();
-});
-
-let loginWin: null | BrowserWindow = null;
-ipcMain.handle(EVENT.LOGIN, async () => {
-  if (loginWin) {
-    loginWin.focus();
-  } else {
-    loginWin = new BrowserWindow({
-      width: 320,
-      height: 520,
-      title: '登录',
-      resizable: false,
-      frame: false,
-      minimizable: false,
-      maximizable: false,
-      parent: win,
-      webPreferences: {
-        preload: preloadLogin,
-      },
-    });
-    if (process.env.VITE_DEV_SERVER_URL) {
-      await loginWin.loadURL(url + '/login/');
-      loginWin.webContents.openDevTools();
-    } else {
-      loginWin.loadFile(loginHtml);
-    }
-    loginWin.on('closed', () => {
-      loginWin = null;
-    });
-  }
-});
-ipcMain.handle(EVENT.LOGIN_CLOSE, () => {
-  loginWin.close();
-  loginWin = null;
-});
-
-ipcMain.handle(EVENT.RELOAD_USER, (_, args) => {
-  return win.webContents.executeJavaScript('window.loadUser()');
-});
+new Main();
---
+++ electron/main/login.ts
@@ -0,0 +1,48 @@
+import { BrowserWindow, ipcMain } from 'electron';
+import { join } from 'node:path';
+import { EVENT } from '../utils/eventTypes';
+
+const url = process.env.VITE_DEV_SERVER_URL;
+const preloadLogin = join(__dirname, '../preload/login.js');
+const loginHtml = join(process.env.DIST, '/login/index.html');
+
+export class Login {
+  static win: BrowserWindow | null = null;
+  constructor(options) {
+    this.createWindow(options);
+  }
+  private async createWindow({ parent }) {
+    if (Login.win) {
+      Login.win.focus();
+      return;
+    }
+
+    Login.win = new BrowserWindow({
+      width: 320,
+      height: 520,
+      title: '登录',
+      resizable: false,
+      frame: false,
+      minimizable: false,
+      maximizable: false,
+      parent,
+      webPreferences: {
+        preload: preloadLogin,
+      },
+    });
+
+    if (process.env.VITE_DEV_SERVER_URL) {
+      await Login.win.loadURL(url + '/login/');
+      Login.win.webContents.openDevTools();
+    } else {
+      Login.win.loadFile(loginHtml);
+    }
+    return;
+  }
+  static registerHandle() {
+    ipcMain.handle(EVENT.LOGIN_CLOSE, () => {
+      this.win.close();
+      this.win = null;
+    });
+  }
+}
